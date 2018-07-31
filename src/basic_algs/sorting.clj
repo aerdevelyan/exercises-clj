89e1bbfdf553a0ca723338abd0475eb87b6e253e
Armen Erdevelyan <armen.erdevelyan@gmail.com>
2018-07-31 23:35:03 +0400

    more efficient impl of quick sort

--- src/basic_algs/sorting.clj
+++ src/basic_algs/sorting.clj
@@ -7,8 +7,9 @@
     (= 1 (count items)) items
     :default
     (let [pivot (first items)
-          lesser-items (filter #(< % pivot) (rest items))
-          gtoe-items (filter #(>= % pivot) (rest items))]
-       (vec (flatten [(qsort lesser-items) pivot (qsort gtoe-items)])))))
+          tparts (transient {:ltp [] :gtp []})
+          sep-fn (fn [res item] (let [k (if (< item pivot) :ltp :gtp)] (assoc! res k (conj (k res) item))))
+          parts (persistent! (reduce sep-fn tparts (rest items)))]
+        (vec (flatten [(qsort (:ltp parts)) pivot (qsort (:gtp parts))])))))
 
 (qsort [10 8 15 5 76 43 15 77 99 76 41])
